fb4a08beed0129ef7fa7b7b650309255f410548c
Kovach <kovach@webthink.com>
2018-02-02 02:26:21 +0100

    another board

---
+++ zenzor2/zenzoduino/zenzoduino.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+
+#define MQ9_PIN     A0
+#define VOC_PIN     A1
+#define RX_PIN      7
+#define TX_PIN      8
+
+SoftwareSerial toesp(RX_PIN, TX_PIN);
+
+void setup() {
+  Serial.begin(115200);
+
+  toesp.begin(4800);
+}
+
+void loop() {
+
+  float mq9 = analogRead(MQ9_PIN);
+  Serial.print("G=");
+  Serial.println(mq9);
+  toesp.print("G=");
+  toesp.println(mq9);
+
+  float voc = analogRead(VOC_PIN);
+  Serial.print("V=");
+  Serial.println(voc);
+  toesp.print("V=");
+  toesp.println(voc);
+
+  delay(1000);
+}
--- zenzor2/zenzor2.ino
+++ zenzor2/zenzor2.ino
@@ -1,11 +1,8 @@
-#include <PMS.h>
 #include <DHT.h>
-#include <Adafruit_GFX.h>    
-#include <Adafruit_ST7735.h> 
-#include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
+#include <SoftwareSerial.h>
 
 
 #define WLAN_SSID       "KIKA"
@@ -19,55 +16,29 @@
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_USERNAME, MQTT_KEY);
 
-Adafruit_MQTT_Subscribe feedScreen = Adafruit_MQTT_Subscribe(&mqtt, "haklab/hardware/tinytft");
-Adafruit_MQTT_Publish feedDust = Adafruit_MQTT_Publish(&mqtt, "haklab/hardware/dust");
-Adafruit_MQTT_Publish feedHumid = Adafruit_MQTT_Publish(&mqtt, "haklab/hardware/humid");
-Adafruit_MQTT_Publish feedTemp = Adafruit_MQTT_Publish(&mqtt, "haklab/hardware/temp");
+Adafruit_MQTT_Publish feedHumid = Adafruit_MQTT_Publish(&mqtt, "k0doma/vlazhnost");
+Adafruit_MQTT_Publish feedTemp = Adafruit_MQTT_Publish(&mqtt, "k0doma/toplina");
+Adafruit_MQTT_Publish feedGas = Adafruit_MQTT_Publish(&mqtt, "k0doma/combustible");
+Adafruit_MQTT_Publish feedVoc = Adafruit_MQTT_Publish(&mqtt, "k0doma/volatile");
 
 
-#define DHTPIN 4     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTPIN   D1      // what pin we're connected to
+#define DHTTYPE  DHT22   // DHT 22  (AM2302)
 
 DHT dht(DHTPIN, DHTTYPE);
-/*
- * ESP8266-12        HY-1.8 SPI
- * GPIO5             Pin 06 (RESET)
- * GPIO2             Pin 07 (A0)
- * GPIO13 (HSPID)    Pin 08 (SDA)
- * GPIO14 (HSPICLK)  Pin 09 (SCK)
- * GPIO15 (HSPICS)   Pin 10 (CS)
-  */
-/*
-#define TFT_PIN_CS   15
-#define TFT_PIN_DC   2
-#define TFT_PIN_RST  5
-*/
-#define TFT_PIN_CS   D0
-#define TFT_PIN_DC   D1
-#define TFT_PIN_RST  10
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_PIN_CS, TFT_PIN_DC, TFT_PIN_RST);
+#define ARDU_RX_PIN      D6
+#define ARDU_TX_PIN      D7
 
-PMS pms(Serial);
-PMS::DATA data;
+SoftwareSerial arduserial(ARDU_RX_PIN, ARDU_TX_PIN);
 
 bool MQTT_connect();
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); 
+  Serial.begin(115200); 
+  arduserial.begin(4800);
   dht.begin();
-  
-  tft.initR(INITR_BLACKTAB);  // You will need to do this in every sketch
-  tft.fillScreen(ST7735_WHITE); 
- 
-
-  //tft print function!
-  tft.setTextColor(ST7735_BLACK);
-  tft.setTextSize(1);
-  tft.setCursor(0,0);
-  //tft.println("Hello World!");  
-  delay(500);
   
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
@@ -79,82 +50,75 @@ void setup() {
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
 }
 
-int screenRow = 0;
 int loopsForPing = 0;
 const int avgLoops = 60;
 float temps[avgLoops];
 float humids[avgLoops];
-float pm1s[avgLoops];
-float pm25s[avgLoops];
-float pm10s[avgLoops];
+float gass[avgLoops];
+float vocs[avgLoops];
 int looper = 0;
+float prevGas = 0;
+float prevVoc = 0;
 
 void loop() {
   bool mqttconnected = MQTT_connect();
 
   float h = dht.readHumidity();
-  // Read temperature as Celsius
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t))
     ESP.restart();
 
-  tft.print("Humidity: "); 
-  tft.print(h);
   humids[looper] = h;
-
-  tft.println("%");
-  tft.print("Temperature: "); 
-  tft.print(t);
-  tft.println("*C");
   temps[looper] = t;
 
-  bool pmsgood = false;
-  if (pms.read(data, 500))
-  {
-    pmsgood = true;
-    
-    float dust1 = data.PM_AE_UG_1_0;
-    tft.print("PM 1.0 (ug/m3): ");
-    tft.println(dust1);
-    pm1s[looper] = dust1;
+  Serial.print("H=");
+  Serial.println(h);
+  Serial.print("T=");
+  Serial.println(t);
 
-    float dust25 = data.PM_AE_UG_2_5;
-    tft.print("PM 2.5 (ug/m3): ");
-    tft.println(dust25);
-    pm25s[looper] = dust25;
+  float gas = prevGas;
+  float voc = prevVoc;
+  String line = arduserial.readString();
+  //Serial.println(line);
+  line.trim();
+  int lidx = line.indexOf('\n');
+  if (lidx > -1) {
+    String line1 = line.substring(0, lidx);
+    line1.trim();
+    Serial.print("L1= ");
+    Serial.println(line1);
 
-    float dust10 = data.PM_AE_UG_10_0;
-    tft.print("PM 10.0 (ug/m3): ");
-    tft.println(dust10);
-    pm10s[looper] = dust10;  
+    int l2idx = line.indexOf('\n', lidx+1);
+    if (l2idx > lidx) {
+      String line2 = line.substring(lidx+1, l2idx);
+      line2.trim();
+      Serial.print("L2= ");
+      Serial.println(line2);
+
+      if (line1.charAt(0) == 'G')
+        parseGasVoc(line1, line2, &gas, &voc);
+      else if (line1.charAt(0) == 'V')
+        parseGasVoc(line2, line1, &gas, &voc);
+    }
   }
-
+  Serial.print("G=");
+  Serial.println(gas);
+  Serial.print("V=");
+  Serial.println(voc);
+  gass[looper] = gas;
+  vocs[looper] = voc;
+  
   looper++;
   if (looper == avgLoops) {
     if (mqttconnected) {
-      tft.println("Sending data to MQTT");
       feedTemp.publish(getaverage(temps));
       feedHumid.publish(getaverage(humids));
-      if (pmsgood) {
-        feedDust.publish(strplusfloat("PM1.0 ", getaverage(pm1s)));
-        feedDust.publish(strplusfloat("PM2.5 ", getaverage(pm25s)));
-        feedDust.publish(strplusfloat("PM10 ", getaverage(pm10s)));
-      }
+      feedGas.publish(getaverage(gass));
+      feedVoc.publish(getaverage(vocs));
     }
     looper = 0;
   }
-  
-  tft.println("");
-  delay(1000);
-
-  screenRow = screenRow + 1;
-  if (screenRow == 3) {
-    tft.fillScreen(ST7735_WHITE); 
-    tft.setCursor(0,0);
-    screenRow = 0;
-  }
-  
 
   // ping the server to keep the mqtt connection alive
   if (loopsForPing > 30) {
@@ -163,6 +127,21 @@ void loop() {
     loopsForPing = 0;
   }
   loopsForPing++;
+}
+
+void parseGasVoc(String linegas, String linevoc, float *gas, float *voc) {
+  if (linegas.charAt(0) == 'G' && linegas.charAt(1) == '=') {
+    String gasval = linegas.substring(2);
+    float tmpgas = gasval.toFloat();
+    if (!isnan(tmpgas) && tmpgas != 0)
+      *gas = tmpgas;
+  }
+  if (linevoc.charAt(0) == 'V' && linevoc.charAt(1) == '=') {
+    String vocval = linevoc.substring(2);
+    float tmpvoc = vocval.toFloat();
+    if (!isnan(tmpvoc) && tmpvoc != 0)
+      *voc = tmpvoc;
+  }  
 }
 
 
